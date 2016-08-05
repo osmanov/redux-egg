b116b43c0a5ce4e6ed04d22cbf36e10c38bc8083
Renat Osmanov <rhenium.osmium@gmail.com>
2016-08-05 11:46:20 +0300

    test:simulate increment/decrement

--- src/components/Counter/index.test.js
+++ src/components/Counter/index.test.js
@@ -1,14 +1,34 @@
 import test from 'ava'
 import React from 'react'
 import {createStore} from 'redux';
-import { shallow, mount } from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import {counterReducer} from '../../reducers/index.js';
 import Counter from './index.js';
 
-test('shallow', t => {
-  let store=createStore(counterReducer);
-  const value=store.getState()+"";
-  const wrapper = shallow(<Counter value={value}/>);
-  t.is(wrapper.contains(<h1>0</h1>), true)
+test('init value should be 0', t => {
+  let store = createStore(counterReducer);
+  const wrapper = mount(<Counter value={store.getState()}/>);
+  wrapper.setProps({value: store.getState()});
+  t.is(wrapper.props().value, 0);
 });
+
+
+test('simulates increment click', t => {
+  let store = createStore(counterReducer);
+  const wrapper = mount(<Counter value={store.getState()} onIncrement={()=>store.dispatch({type:'INCREMENT'})}/>);
+  wrapper.find('button').first().simulate('click');
+  wrapper.setProps({value: store.getState()});
+  t.is(wrapper.props().value, 1);
+});
+
+test('simulates decrement click', t => {
+  let store = createStore(counterReducer, 1);
+  const wrapper = mount(<Counter value={store.getState()} onDecrement={()=>store.dispatch({type:'DECREMENT'})}/>);
+  wrapper.find('button').last().simulate('click');
+  wrapper.setProps({value: store.getState()});
+  t.is(wrapper.props().value, 0);
+});
+
+
+
 
